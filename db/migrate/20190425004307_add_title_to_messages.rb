5bcb49d1c60290c19bdac74ed91706b4b46acf1e
shuya.takane <shuya.takane@hit-inc.jp>
2019-04-25 09:25:53 +0000

    add column

--- db/migrate/20190425004307_add_title_to_messages.rb
+++
@@ -1,5 +0,0 @@
-class AddTitleToMessages < ActiveRecord::Migration[5.2]
-  def change
-    add_column :messages, :title, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_24_011938) do
+ActiveRecord::Schema.define(version: 2019_04_25_082702) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "status"
   end
 
 end
